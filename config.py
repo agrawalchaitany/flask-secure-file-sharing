04117caa6c4837767fae31bda18f44b8050f4d0e
Chaitany Agrawal <60360587+agrawalchaitany@users.noreply.github.com>
2025-02-19 01:22:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
     MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = "gaqtgshtrcmqwbqd"
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWARD')
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_USERNAME')
     
     # Remove quotes if they exist in the environment variables
